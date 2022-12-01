66dff3524450a5dbe87f3002255ba8ec89ebaaa2
Intronirisme <intronirisme@protonmail.com>
2022-12-01 09:38:40 +0100

    implementing juciness

--- Assets/Script/Player/Character.cs
+++ Assets/Script/Player/Character.cs
@@ -7,28 +7,39 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public class Character : MonoBehaviour
 {
-    [Header("Movement")]
+    [Header("Walking")]
     public float Speed = 5f;
     public float TurnRate = .5f;
     public float LookupRate = .5f;
+    public float GroundDamping = 2f;
+
+    [Header("Jumping")]
     public float JumpSpeed = 3f;
-    public float GroundDamping = .1f;
+    public float JumpDuration = .8f;
+    public float MaxFallingSpeed = 4f;
+    public float GravityAcceleration = 9.81f;
 
     [Header("Air control")]
     public float AirControl = .3f;
-    public float AirDamping = .01f;
-    public float GravitySpeed = 10f;
+    public float AirDamping = .5f;
 
-    public bool isGrounded = false;
+
     //references vers les autres composants
     private CharacterController _controls;
-    private GameObject _cam_root;
     private GameObject _body;
+    private CapsuleCollider _caps;
+    private GameObject _cam_root;
     private GameObject _cam;
 
+    //inputs & déplacements
     private Vector2 _moveInput; //récupère l'input du joueur
+    private Vector2 _lookInput;
     private Vector3 _playerMove; //input converti en déplacement
+    private float _iJumping = 0.0f;
     private Vector3 _physicMove; //déplacements causés par la physique
+
+    //booleen ayant pour but d'éviter l'input bouncing
+    private bool _iInteracting = false;
 
     // Start is called before the first frame update
     void Start()
@@ -36,6 +47,7 @@ public class Character : MonoBehaviour
         //assignations des références vers nos autres composants
         _controls = gameObject.GetComponent<CharacterController>();
         _body = gameObject.FindInChildren("Body");
+        _caps = _body.GetComponent<CapsuleCollider>();
         _cam_root = gameObject.FindInChildren("CameraRoot");
         _cam = _cam_root.FindInChildren("Camera");
 
@@ -47,64 +59,87 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isGrounded = IsGrounded();
-        Vector3 frameMovements = Vector3.zero;
+        float damping = _controls.isGrounded ? GroundDamping : AirDamping;
 
-        float damping = isGrounded ? GroundDamping : AirDamping;
-        float speed = isGrounded ? Speed : Speed * AirControl;
+        GetPlayerMove();
+        UpdateCamera();
+        GetPhysicMove();
 
-        //transforme l'input dans le repère du joueur
-        Vector2 playerSpaceInput = _moveInput.Rotate(_cam_root.transform.rotation.y);
-        
-        _playerMove = new Vector3(
-            playerSpaceInput.x * speed * Time.deltaTime,
-            0,
-            playerSpaceInput.y * speed * Time.deltaTime
-        );
-
-
+        Vector3 frameMovements = _playerMove + _physicMove;
+        frameMovements -= frameMovements * damping;
         // applique tout les movements
-        frameMovements = _playerMove + _physicMove;
         _controls.Move(frameMovements);
     }
 
-    public bool IsGrounded()
+    private void GetPlayerMove()
     {
-        RaycastHit groundHit;
-        Vector3 rayStart = gameObject.transform.position;
+        float moveSpeed = _controls.isGrounded ? Speed : Speed * AirControl;
+        //transforme l'input dans le repère du joueur
+        Vector2 playerSpaceInput = _moveInput.Rotate(_cam_root.transform.rotation.y);
 
-        //récupère le rayon de Body (à l'échelle)
-        float radiusUnscaled = _body.GetComponent<CapsuleCollider>().radius;
-        float bodyScaleFactor = _body.transform.localScale.x;
-        float scaledRadius = radiusUnscaled * bodyScaleFactor;
+        _playerMove = new Vector3
+        (
+            playerSpaceInput.x * moveSpeed * Time.deltaTime,
+            0,
+            playerSpaceInput.y * moveSpeed * Time.deltaTime
+        );
+    }
 
-        //récupère la hauteur du nombril aux pieds de Body (à l'échelle)
-        float halfHeightUnscale = _body.GetComponent<CapsuleCollider>().height / 2;
-        float bodyScaleFactorY = _body.transform.localScale.y;
-        float scaledHalfHeight = halfHeightUnscale * bodyScaleFactorY;
+    private void GetPhysicMove()
+    {
+        float gravityMove = _controls.isGrounded ? 0.0f : -GravityAcceleration * Time.deltaTime;
+        float jumpMove = _iJumping > 0.0f ? JumpSpeed * Time.deltaTime : 0.0f;
 
-        //marge de détection
-        float threshold = 0.05f;
+        _physicMove += new Vector3(0, _iJumping > 0.0f ? jumpMove : gravityMove, 0);
+        _iJumping = Mathf.Clamp(_iJumping - Time.deltaTime, 0.0f, JumpDuration);
+    }
 
-        return Physics.SphereCast(rayStart, scaledRadius, new Vector3(0, -1, 0), out groundHit, scaledHalfHeight+threshold);
+    private void UpdateCamera()
+    {
+        baseRotation = new Quaternion
+        (
+            _cam_root.transform.rotation.x,
+            _cam_root.transform.rotation.y,
+            _cam_root.transform.rotation.z
+        );
+
+        //baseRotation 
     }
 
     public void OnJump(InputAction.CallbackContext value)
     {
-        //lire des input binaire (pressé / relâché)
-        bool _iJumping = value.ReadValueAsButton();
-        Debug.Log("On Jump ! => " + _iJumping);
+        //lire des input binaire (pressé / relâché) en évitant le rebond
 
-        if(value.ReadValueAsButton())
+        if(value.ReadValueAsButton() && _controls.isGrounded)
         {
-            //si barre espace pressée
+            Debug.Log("Jump");
+        }
+        else if(!value.ReadValueAsButton())
+        {
+            _iJumping = 0.0f;
+            Debug.Log("No Jump");
         }
     }
 
-        public void OnMove(InputAction.CallbackContext value)
+    public void OnInteract(InputAction.CallbackContext value)
+    {
+        //lire des input binaire (pressé / relâché) en évitant le rebond
+
+        if(value.ReadValueAsButton() && !_iInteracting) {
+            _iInteracting = true;
+        } else if(!value.ReadValueAsButton() && _iInteracting) {
+            _iInteracting = false;
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext value)
     {
         //value.ReadValue<Vector2>() lire un vecteur
         _moveInput = value.ReadValue<Vector2>();
-        Debug.Log("On Move ! => " + _moveInput);
+    }
+
+    public void OnLook(InputAction.CallbackContext value)
+    {
+        _lookInput = value.ReadValue<Vector2>();
     }
 }
